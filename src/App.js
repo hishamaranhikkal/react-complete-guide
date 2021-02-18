e13e69a2ba5309167d764995fbee45a33e324f2e
unknown <hamppm@gmail.com>
2021-02-18 16:38:29 +0530

    after dynamic allocation of name

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import { render } from '@testing-library/react';
 class App extends Component{
   state={
       person:[
-        {name:"hisham1", age:40},
-        {name:"afam1", age:50},
-        {name:"shilpa1", age:60}
+        {name:"hisham1", age:40,job:"django student"},
+        {name:"afam1", age:50,job:"python student"},
+        {name:"shilpa1", age:60,job:"react student"}
       ]
   }
   render(){
@@ -17,10 +17,10 @@ class App extends Component{
     <div className="App">
       <h1>react app is working</h1>
       <p>hey folks!!!</p>
-      <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
+      <Person name={this.state.person[0].name} age={this.state.person[0].age} job={this.state.person[0].job}></Person>
       <p>person using times</p>
-      <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
-      <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
+      <Person name={this.state.person[1].name} age={this.state.person[0].age} job={this.state.person[1].job}></Person>
+      <Person name={this.state.person[2].name} age={this.state.person[0].age} job={this.state.person[2].job}></Person>
     </div>
     );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import reactDom from "react-dom";
 import React from 'react';
 const person = (props)=>{
-    return <p>Its inside the person name is {props.name} i am {props.age} age</p>
+    return <p>Its inside the person name is {props.name} i am {props.age} age and job is {props.job}</p>
 
 };
 
