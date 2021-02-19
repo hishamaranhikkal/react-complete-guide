439e9da0aa846f6abacfd812d791eae3238802fa
unknown <hamppm@gmail.com>
2021-02-19 11:33:16 +0530

    person chane on button click

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import logo from './logo.svg';
-import React,{Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 import { render } from '@testing-library/react';
-class App extends Component{
-    state={
+const App = props =>{
+  const[personState,setPersonState]= useState({
         person:[
           {name:"hisham1", age:40,job:"django student"},
           {name:"afam1", age:50,job:"python student"},
           {name:"shilpa1", age:60,job:"react student"}
         ]
-    }
-    swicthNameHandler = () =>{
-      console.log("button clicked from switch handler function");
-      
-    }
-    render(){
+    });
+    const swicthNameHandler = () =>{
+      // console.log("button clicked from switch handler function");
+      setPersonState({
+        person:[
+          {name:"hisham1 from function", age:40,job:"django student from function"},
+          {name:"afam1 from function", age:50,job:"python student from function"},
+          {name:"shilpa1 from function", age:60,job:"react student from function"}
+        ]
+      });
+    };
+    // render(){
       return (
-        <div className="App">
-          <h1>react app is working</h1>
-          <p>hey folks!!!</p>
-          <Person name={this.state.person[0].name} age={this.state.person[0].age} job={this.state.person[0].job}></Person>
-          <p>person using times</p>
-          <button onClick={this.swicthNameHandler}>Switch</button>
-          <Person name={this.state.person[1].name} age={this.state.person[0].age} job={this.state.person[1].job}></Person>
-          <Person name={this.state.person[2].name} age={this.state.person[0].age} job={this.state.person[2].job}></Person>
-        </div>
+          <div className="App">
+            <h1>react app is working</h1>
+            <p>hey folks!!!</p>
+            <Person name={personState.person[0].name} age={personState.person[0].age} job={personState.person[0].job}></Person>
+            <p>person using times</p>
+            <button onClick={swicthNameHandler}>Switch</button>
+            <Person name={personState.person[1].name} age={personState.person[0].age} job={personState.person[1].job}></Person>
+            <Person name={personState.person[2].name} age={personState.person[0].age} job={personState.person[2].job}></Person>
+          </div>
         );
-    }
-}
+    
+};
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import reactDom from "react-dom";
 import React from 'react';
-const person = (props)=>{
+const Person = (props)=>{
     return <p>Its inside the person name is {props.name} i am {props.age} age and job is {props.job}</p>
 
 };
 
-export default person;
+export default Person;
