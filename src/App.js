e47eef02bdd1061f401e5d62d2175c06f18f2899
unknown <hamppm@gmail.com>
2021-02-19 10:33:20 +0530

    dynamic allocation of person1

--- src/App.js
+++ src/App.js
@@ -5,25 +5,30 @@ import Person from './Person/Person';
 
 import { render } from '@testing-library/react';
 class App extends Component{
-  state={
-      person:[
-        {name:"hisham1", age:40,job:"django student"},
-        {name:"afam1", age:50,job:"python student"},
-        {name:"shilpa1", age:60,job:"react student"}
-      ]
-  }
-  render(){
-  return (
-    <div className="App">
-      <h1>react app is working</h1>
-      <p>hey folks!!!</p>
-      <Person name={this.state.person[0].name} age={this.state.person[0].age} job={this.state.person[0].job}></Person>
-      <p>person using times</p>
-      <Person name={this.state.person[1].name} age={this.state.person[0].age} job={this.state.person[1].job}></Person>
-      <Person name={this.state.person[2].name} age={this.state.person[0].age} job={this.state.person[2].job}></Person>
-    </div>
-    );
-}
+    state={
+        person:[
+          {name:"hisham1", age:40,job:"django student"},
+          {name:"afam1", age:50,job:"python student"},
+          {name:"shilpa1", age:60,job:"react student"}
+        ]
+    }
+    swicthNameHandler = () =>{
+      console.log("button clicked from switch handler function");
+      
+    }
+    render(){
+      return (
+        <div className="App">
+          <h1>react app is working</h1>
+          <p>hey folks!!!</p>
+          <Person name={this.state.person[0].name} age={this.state.person[0].age} job={this.state.person[0].job}></Person>
+          <p>person using times</p>
+          <button onClick={this.swicthNameHandler}>Switch</button>
+          <Person name={this.state.person[1].name} age={this.state.person[0].age} job={this.state.person[1].job}></Person>
+          <Person name={this.state.person[2].name} age={this.state.person[0].age} job={this.state.person[2].job}></Person>
+        </div>
+        );
+    }
 }
 
 export default App;
